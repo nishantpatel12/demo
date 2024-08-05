54ea39d772de667aad237a2875d7f696cf6778a1
nishantpatel12 <nishant.patel@liseinfotech.com>
2024-08-06 01:15:32 +0530

    second commit

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+class Student {
+    constructor(marks) {
+        this.marks = marks;
+      }
+    }
+    
+    const students = [
+        new Student(85),
+        new Student(50),
+        new Student(75),
+        new Student(90),
+        new Student(45)
+    ];
+
+
+    for(let i=0;i<students.length;i++){
+       console.log(students[i])
+
+    }
