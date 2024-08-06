7a26bdf0d9f2595db35abdad839df23c532854ff
nishantpatel12 <nishant.patel@liseinfotech.com>
2024-08-06 17:35:37 +0530

    second commit file 2

--- test2.js
+++ test2.js
@@ -1,1 +1,18 @@
-console.log("hello")
+class person {
+constructor(name,age){
+    this.name=name;
+    this.age=age;   
+}
+    works(){
+        console.log("works 12hr")
+    }
+
+}
+class student extends person{
+    study(){
+        console.log("studys 4hr")
+    }
+}
+
+const nishantobj = new person("Nishant",22) 
+console.log(nishantobj)
